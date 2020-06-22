7e43b33657145e16a5c7f6b2aa59edeedb7e3ac0
AugustineCervantes <Augustine.CervantesIII@gmail.com>
2020-06-22 16:55:19 -0500

    adding contacts, people, and list files with functionality

---
+++ contacts-manager-exercise/src/People.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class People {
+
+    private String name;
+
+    public People(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public static List<People> nameStringsToPeople(List<String> contacts) {
+        List<People> instructors = new ArrayList<>();
+        for (String name : contacts) {
+            instructors.add(new People(name));
+        }
+        return instructors;
+    }
+
+}
